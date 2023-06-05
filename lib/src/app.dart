9cfe4846d44b27f7a6535851cc071b6948e8fe5f
pratikpwr <pratiksatishpawar@gmail.com>
2023-06-05 23:00:53 +0530

    minor changes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AV Account APP',
       theme: AppTheme.theme,
       debugShowCheckedModeBanner: false,
       home: const MyAccountScreen(),
